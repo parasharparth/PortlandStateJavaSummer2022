0900c124187d8fd15cae6145623a5b754dbf98af
vidyav2 <vidyav2@pdx.edu>
2022-07-07 03:42:20 -0700

    Added the first project files

---
+++ phonebill/src/main/java/edu/pdx/cs410J/vidyav2/PhoneCall.java
@@ -0,0 +1,25 @@
+package edu.pdx.cs410J.vidyav2;
+
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+public class PhoneCall extends AbstractPhoneCall {
+  @Override
+  public String getCaller() {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+
+  @Override
+  public String getCallee() {
+    return "This method is not implemented yet";
+  }
+
+  @Override
+  public String getBeginTimeString() {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+
+  @Override
+  public String getEndTimeString() {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+}
