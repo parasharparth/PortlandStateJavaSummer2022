7fefc0389a4457614092acc26b546abfca357598
vidyav2 <hjvidyavarshini@gmail.com>
2022-08-13 21:17:31 -0700

    Project 5 Final

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/CreateNewPhoneCallActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/CreateNewPhoneCallActivity.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import edu.pdx.cs410J.ParserException;
@@ -32,7 +33,9 @@ public class CreateNewPhoneCallActivity extends AppCompatActivity {
         EditText endDateTime = findViewById(R.id.endDateTime);
 
         String[] beginDateTimeWords = beginDateTime.getText().toString().split(" ");
+        System.out.println("print date 1" + Arrays.toString(beginDateTimeWords));
         String[] endDateTimeWords = endDateTime.getText().toString().split(" ");
+        System.out.println("print date 2" + Arrays.toString(endDateTimeWords));
 
         if(customer.getText().toString().equals("")){
             Snackbar.make(view, "Enter valid Customer name", 1000).show();
@@ -46,31 +49,15 @@ public class CreateNewPhoneCallActivity extends AppCompatActivity {
             Snackbar.make(view, "Enter correct callee number", 1000).show();
             return;
         }
-        if(beginDateTimeWords.length == 3) {
-            if (!checkdatetime(beginDateTimeWords[0], beginDateTimeWords[1], beginDateTimeWords[2])) {
-                Snackbar.make(view, "Enter valid begin date and time", 1000).show();
-                return;
-            }
-        }
-
-        if(endDateTimeWords.length == 3) {
-            if (!checkdatetime(endDateTimeWords[0], endDateTimeWords[1], endDateTimeWords[2])) {
-                Snackbar.make(view, "Enter valid end date time", 1000).show();
-                return;
-            }
-        }
-        else { Snackbar.make(view, "Enter valid end date time", 1000).show(); return;  }
 
         PhoneCall call = new PhoneCall();
+        call.setCallerName(customer.getText().toString());
         call.setCallerNumber(callerNumber.getText().toString());
         call.setCalleeNumber(calleeNumber.getText().toString());
+        call.setPhoneCallBeginDate(beginDateTimeWords[0]);
         call.setPhoneCallBeginTime(beginDateTimeWords[0], beginDateTimeWords[1], beginDateTimeWords[2]);
+        call.setPhoneCallEndDate(endDateTimeWords[0]);
         call.setPhoneCallEndTime(endDateTimeWords[0], endDateTimeWords[1], endDateTimeWords[2]);
-
-        if(!call.chkEndAfterStart()){
-            Snackbar.make(view, "Error: Begin Time is before End Time", 1000).show();
-            return;
-        }
 
         TextParser parser = new TextParser(dir, customer.getText().toString() + ".txt", customer.getText().toString());
         PhoneBill parsedphonebill  = parser.parse();
@@ -78,12 +65,13 @@ public class CreateNewPhoneCallActivity extends AppCompatActivity {
         TextDumper dumper = new TextDumper(dir, customer.getText().toString() + ".txt");
         dumper.dump(parsedphonebill);
 
+
         customer.setText("");
         callerNumber.setText("");
         calleeNumber.setText("");
         beginDateTime.setText("");
         endDateTime.setText("");
-        Snackbar.make(view, "Phonecall Added Successfully", 1000).show();
+        Snackbar.make(view, "Phonecall Added Successfully. Please search the phonecall details in SEARCH PHONECALL tab.", 3000).show();
     }
 
     /**
@@ -93,7 +81,7 @@ public class CreateNewPhoneCallActivity extends AppCompatActivity {
      * @param ampm The time of the day (am or pm)
      */
     public static boolean checkdatetime(String date, String time, String ampm) {
-        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm am");
         String finaldatetime = date + " " + time + " " + ampm;
         try{
             Date d = formatter.parse(finaldatetime);
--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/TextParser.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/TextParser.java
@@ -1,11 +1,8 @@
 package edu.pdx.cs410j.whitlock;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.PrintWriter;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,78 +10,65 @@ import edu.pdx.cs410J.ParserException;
 import edu.pdx.cs410J.PhoneBillParser;
 
 public class TextParser implements PhoneBillParser<PhoneBill> {
-    private final Reader reader;
 
-    public String filename;
+    public String filename, customerName;
+    File dir;
 
-    public String customerName;
-    public String billDetails;
-
-//    public TextParser(Reader reader) {
-//        this.reader = reader;
-//    }
-
-    public TextParser(File dir, String filename, String customerName)
+    public TextParser(File directory, String filename, String customer)
     {
         this.filename = filename;
-        this.customerName = customerName;
-        this.reader = null;
+        this.customerName = customer;
+        this.dir = directory;
     }
 
     @Override
     public PhoneBill parse() throws ParserException {
         Scanner sc = null;
         try {
-            File input = new File(this.filename);
-            BufferedReader br = new BufferedReader(new FileReader((input)));
-        } catch (Exception e)
+            sc = new Scanner(new File(dir, this.filename));
+        } catch (FileNotFoundException e)
         {
             System.out.println("Text Dump File with given name does not exist. New File created.");
-            PrintWriter out = null;
-            //out = new PrintWriter(this.filename);
-            if (!this.filename.contains("/")) {
+            if(!this.filename.contains("/")){
+                PrintWriter out = null;
                 try {
-                    out = new PrintWriter(this.filename);
+                    out = new PrintWriter(new File(dir, this.filename));
                 } catch (FileNotFoundException ex) {
                     System.out.println("File is not present.");
                 }
-                // out.write(this.customerName);   //print customer name here
-//                System.out.println("I was here");
                 out.write(this.customerName);
-                out.flush();
                 out.close();
             }
             else {
+                PrintWriter out = null;
                 File f = null;
                 File f1 = null;
                 String v;
                 boolean bool = false;
-                f = new File(this.filename);
+                f = new File(dir, this.filename);
                 f1 = f.getParentFile();
                 v = f1.getAbsolutePath();
+                bool = f1.exists();
                 //check if directory exists or not
-                if (f1.exists()) {
-                    //System.out.println("debug 2");
+                if (bool) {
                     try {
-                        out = new PrintWriter(f);
-                        //System.out.println("debug 3");
+                        out = new PrintWriter(new File(dir, this.filename));
                     } catch (FileNotFoundException ex) {
                         ex.printStackTrace();
                     }
                     out.write(this.customerName);
-                    out.flush();
+//                    out.flush();
                     out.close();
                 } else {
                     File folder = new File(v);
                     if (folder.mkdir()) {
                         try {
-                            out = new PrintWriter(this.filename);
+                            out = new PrintWriter(new File(dir, this.filename));
                         } catch (FileNotFoundException ex) {
                             ex.printStackTrace();
                         }
                         out.write(this.customerName);
-                        out.flush();
-                        //System.out.println("debug1");
+                        //out.flush();
                         out.close();
                     } else {
                         System.out.println("Could not create directory");
@@ -94,16 +78,15 @@ public class TextParser implements PhoneBillParser<PhoneBill> {
             }
         }
         try {
-            sc = new Scanner(new File(this.filename));
+            sc = new Scanner(new File(dir, this.filename));
         } catch (FileNotFoundException e) {
             System.out.println("File is not present.");
         }
 
         ArrayList<String> lines = new ArrayList<String>();
-
+        assert sc != null;
         while (sc.hasNextLine()) {
             lines.add(sc.nextLine());
-            //System.out.println(""+ lines);
         }
         PhoneBill bill = new PhoneBill(this.customerName);
         if(lines.size() == 0)
@@ -122,7 +105,7 @@ public class TextParser implements PhoneBillParser<PhoneBill> {
                 System.out.println("txt is" + first_name[0]);
             }
         }
-        for(int i=1;i<lines.size(); i++) {/*
+        for(int i=1;i<lines.size(); i++) {
             //System.out.println("I was here");
             String[] words = lines.get(i).split(" ");
 //            if(words.length!=12)
@@ -146,7 +129,7 @@ public class TextParser implements PhoneBillParser<PhoneBill> {
 //                System.out.println("This text file cannot be parsed.");
 //                System.exit(0);
 //            }
-            bill.addPhoneCall(call);*/
+            bill.addPhoneCall(call);
         }
 
         return bill;
