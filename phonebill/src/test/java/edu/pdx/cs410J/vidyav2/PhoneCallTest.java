fdad826c2a318c36da645ab619e61279ed640662
vidyav2 <hjvidyavarshini@gmail.com>
2023-03-23 09:06:47 -0700

    Removed hard coded Phone call unit tests.

--- phonebill/src/test/java/edu/pdx/cs410J/vidyav2/PhoneCallTest.java
+++ phonebill/src/test/java/edu/pdx/cs410J/vidyav2/PhoneCallTest.java
@@ -1,11 +1,11 @@
 package edu.pdx.cs410J.vidyav2;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.Date;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.Date;
+//
+//import static org.hamcrest.CoreMatchers.*;
+//import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Unit tests for the {@link PhoneCall} class.
@@ -14,50 +14,50 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class PhoneCallTest {
 
-    String callerNumber = "123-777-9997";
-    String calleeNumber = "888-998-9999";
-
-
-    Date phoneCallBeginTime = new Date("07/07/2021 08:00 PM"), phoneCallEndTime = new Date("07/07/2021 08:05 PM");
-    String callerName = "Bhaskar";
-    String phoneCallBeginDate;
-            //= "07/07/2022";
-    String phoneCallEndDate;
-                    //= "07/07/2022";
-
-  PhoneCall call = new PhoneCall(callerName, callerNumber, calleeNumber, phoneCallBeginDate,phoneCallBeginTime, phoneCallEndDate,phoneCallEndTime);
-
-  /**
-   * This unit test will need to be modified (likely deleted) as you implement
-   * your project.
-   */
-
-  @Test
-  void getCallerIsImplemented() {
-    assertThat(call.getCaller(), equalTo(callerNumber));
-  }
-
-  @Test
-  void getCalleeIsImplemented() {
-    assertThat(call.getCallee(), equalTo(calleeNumber));
-  }
+//    String callerNumber = "123-777-9997";
+//    String calleeNumber = "888-998-9999";
+//
+//
+//    Date phoneCallBeginTime = new Date("07/07/2021 08:00 PM"), phoneCallEndTime = new Date("07/07/2021 08:05 PM");
+//    String callerName = "Bhaskar";
+//    String phoneCallBeginDate;
+//            //= "07/07/2022";
+//    String phoneCallEndDate;
+//                    //= "07/07/2022";
+//
+//  PhoneCall call = new PhoneCall(callerName, callerNumber, calleeNumber, phoneCallBeginDate,phoneCallBeginTime, phoneCallEndDate,phoneCallEndTime);
+//
+//  /**
+//   * This unit test will need to be modified (likely deleted) as you implement
+//   * your project.
+//   */
+//
 //  @Test
-//  void getBeginTimeStringIsImplemented() {
-//    assertThat(call.getBeginTimeString(), equalTo(phoneCallBeginTime));
+//  void getCallerIsImplemented() {
+//    assertThat(call.getCaller(), equalTo(callerNumber));
 //  }
-
+//
 //  @Test
-//  void getEndTimeStringIsImplemented() {
-//    assertThat(call.getEndTimeString(), equalTo(phoneCallEndTime));
+//  void getCalleeIsImplemented() {
+//    assertThat(call.getCallee(), equalTo(calleeNumber));
 //  }
-
-  @Test
-  void initiallyAllPhoneCallsHaveTheSameCallee() {
-    assertThat(call.getCallee(), equalTo(calleeNumber));
-  }
-
-  @Test
-  void forProject1ItIsOkayIfGetBeginTimeReturnsNull() {
-    assertThat(call.getBeginTime(), is(nullValue()));
-  }
+////  @Test
+////  void getBeginTimeStringIsImplemented() {
+////    assertThat(call.getBeginTimeString(), equalTo(phoneCallBeginTime));
+////  }
+//
+////  @Test
+////  void getEndTimeStringIsImplemented() {
+////    assertThat(call.getEndTimeString(), equalTo(phoneCallEndTime));
+////  }
+//
+//  @Test
+//  void initiallyAllPhoneCallsHaveTheSameCallee() {
+//    assertThat(call.getCallee(), equalTo(calleeNumber));
+//  }
+//
+//  @Test
+//  void forProject1ItIsOkayIfGetBeginTimeReturnsNull() {
+//    assertThat(call.getBeginTime(), is(nullValue()));
+//  }
 }
