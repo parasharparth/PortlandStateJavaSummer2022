8caa812c2dfc1aec2ae25e1fb140676b0546173b
parasharparth <parthparashar6@gmail.com>
2023-03-23 04:24:09 -0700

    Project3 redundant code removed

--- phonebill/src/main/java/edu/pdx/cs410J/vidyav2/Project3.java
+++ phonebill/src/main/java/edu/pdx/cs410J/vidyav2/Project3.java
@@ -29,7 +29,7 @@ public class Project3 {
             System.out.println("-README --> Prints a README for this project and exits");
             return;}
 
-        boolean readMeFlag = readMeFlagCheck(args);
+        boolean readMeFlag = HelperFunctions.readMeFlagCheck(args);
         if (readMeFlag) {
             String FileContentReadMe = readFromReadMeFileOnly();
             System.out.println(FileContentReadMe);
@@ -128,81 +128,7 @@ public class Project3 {
             }
 
         }
-        else if (commandLineArgs.size() == 12) {/*
-            if ((commandLineArgs.get(0).contains("-textFile") && commandLineArgs.get(1).contains("-pretty") ||
-                    (commandLineArgs.get(0).equals("-pretty") && commandLineArgs.get(1).equals("-textFile") &&
-                            commandLineArgs.get(2).contains(".txt")) || commandLineArgs.get(0).contains("-print") &&
-                    commandLineArgs.get(1).equals("-pretty") && commandLineArgs.get(2).contains(".txt"))) {
-                int index = 0;
-                for (int i = 0; true; i++) {
-                    if (commandLineArgs.get(i).contains("-pretty")) {
-                        index = i + 2;
-                    }
-                    break;
-                }
-                String fileName = commandLineArgs.get(2);
-                TextDumper dumper = new TextDumper();
-                TextParser parser = new TextParser(fileName, commandLineArgs.get(index));
-                parser.setFilename(fileName);
-                dumper.setFileName(fileName);
-                parser.setCustomerName(commandLineArgs.get(3));
-                PhoneBill bill = parser.parse();
-                PhoneCall call = new PhoneCall();
-                call.setCallerName(commandLineArgs.get(3));
-                call.setCallerNumber(commandLineArgs.get(4));
-                call.setCalleeNumber(commandLineArgs.get(5));
-                call.setPhoneCallBeginDate(commandLineArgs.get(6));
-                call.setPhoneCallBeginTime(commandLineArgs.get(6), commandLineArgs.get(7), commandLineArgs.get(8));
-                call.setPhoneCallEndDate(commandLineArgs.get(9));
-                call.setPhoneCallEndTime(commandLineArgs.get(9), commandLineArgs.get(10), commandLineArgs.get(11));
-                boolean allRequiredArgumentsAreValid = checkValidityOfRequiredArgs(commandLineArgs);
-                if (!allRequiredArgumentsAreValid) {
-                    return;
-                }
-                bill.addPhoneCall(call);
-                dumper.dump(bill);
-                PrettyPrinter printer = new PrettyPrinter();
-                printer.setFilename(commandLineArgs.get(2));
-                printer.setCustomerName(commandLineArgs.get(3));
-                printer.getPretty(call, commandLineArgs.get(4));
-                printer.dump(bill);
-                //System.out.println("\nPretty file is printed. Check the pretty file folder." );
-            }
-            else if ((commandLineArgs.get(0).contains("-pretty") && commandLineArgs.get(1).contains(".txt"))) {
-                for (int i = 0; true; i++) {
-                    if (commandLineArgs.get(i).contains(".txt")) {
-                        int index = i + 2;
-                    }
-                    break;
-                }
-                String fileName = commandLineArgs.get(1);
-                TextDumper dumper = new TextDumper();
-                TextParser parser = new TextParser(fileName, commandLineArgs.get(2));
-                parser.setFilename(fileName);
-                dumper.setFileName(fileName);
-                parser.setCustomerName(commandLineArgs.get(2));
-                PhoneBill bill = parser.parse();
-                PhoneCall call = new PhoneCall();
-                call.setCallerName(commandLineArgs.get(2));
-                call.setCallerNumber(commandLineArgs.get(3));
-                call.setCalleeNumber(commandLineArgs.get(4));
-                call.setPhoneCallBeginDate(commandLineArgs.get(5));
-                call.setPhoneCallBeginTime(commandLineArgs.get(5), commandLineArgs.get(6), commandLineArgs.get(7));
-                call.setPhoneCallEndDate(commandLineArgs.get(8));
-                call.setPhoneCallEndTime(commandLineArgs.get(8), commandLineArgs.get(9), commandLineArgs.get(10));
-                boolean allRequiredArgumentsAreValid = checkValidityOfRequiredArgs(commandLineArgs);
-                if (!allRequiredArgumentsAreValid) {
-                }
-                bill.addPhoneCall(call);
-                dumper.dump(bill);
-                PrettyPrinter printer = new PrettyPrinter();
-                printer.setFilename(commandLineArgs.get(1));
-                printer.setCustomerName(commandLineArgs.get(2));
-                printer.getPretty(call, commandLineArgs.get(2));
-                printer.dump(bill);
-                //System.out.println("\nPretty file is printed. Check the pretty file folder." );
-            }
-        */}
+        else if (commandLineArgs.size() == 12) {}
         else if (commandLineArgs.size() == 13) {
             if ((commandLineArgs.get(0).contains("-textFile") && commandLineArgs.get(1).contains(".txt") &&
                     commandLineArgs.get(2).equals("-pretty") && !commandLineArgs.get(3).equals("-"))) {
@@ -298,11 +224,11 @@ public class Project3 {
             String dateOfPhoneCallEnd = commandLineArgs.get(8);
             String timeOfPhoneCallEnd = commandLineArgs.get(9);
 
-            boolean isCallerNumberValid = checkForValidPhoneNumber(commandLineArgs.get(3));
+            boolean isCallerNumberValid = HelperFunctions.checkForValidPhoneNumber(commandLineArgs.get(3));
             if (!isCallerNumberValid) {
                 return false;
             }
-            boolean isCalleeNumberValid = checkForValidPhoneNumber(commandLineArgs.get(4));
+            boolean isCalleeNumberValid = HelperFunctions.checkForValidPhoneNumber(commandLineArgs.get(4));
             if (!isCalleeNumberValid) {
                 return false;
             }
@@ -320,37 +246,7 @@ public class Project3 {
             }
             return checkForValidPhoneCallTime(commandLineArgs.get(9));
         }
-        else if (commandLineArgs.size() == 12) {/*
-
-            String callerName = commandLineArgs.get(3);
-            String noOfCaller = commandLineArgs.get(4);
-            String noOfCallee = commandLineArgs.get(5);
-            String dateOfPhoneCallBegin = commandLineArgs.get(6);
-            String timeOfPhoneCallBegin = commandLineArgs.get(7);
-            String dateOfPhoneCallEnd = commandLineArgs.get(9);
-            String timeOfPhoneCallEnd = commandLineArgs.get(10);
-
-            boolean isCallerNumberValid = checkForValidPhoneNumber(commandLineArgs.get(4));
-            if (!isCallerNumberValid) {
-                return false;
-            }
-            boolean isCalleeNumberValid = checkForValidPhoneNumber(commandLineArgs.get(5));
-            if (!isCalleeNumberValid) {
-                return false;
-            }
-            boolean isPhoneCallBeginDateValid = checkForValidDate(commandLineArgs.get(6));
-            if (!isPhoneCallBeginDateValid) {
-                return false;
-            }
-            boolean isPhoneCallBeginTimeValid = checkForValidPhoneCallTime(commandLineArgs.get(7));
-            if (!isPhoneCallBeginTimeValid) {
-                return false;
-            }
-            boolean isPhoneCallEndDateValid = checkForValidDate(commandLineArgs.get(9));
-            if (!isPhoneCallEndDateValid) {
-                return false;
-            }
-            return checkForValidPhoneCallTime(commandLineArgs.get(10));*/
+        else if (commandLineArgs.size() == 12) {
         }
         else if (commandLineArgs.size() == 13) {
 
@@ -362,11 +258,11 @@ public class Project3 {
             String dateOfPhoneCallEnd = commandLineArgs.get(10);
             String timeOfPhoneCallEnd = commandLineArgs.get(11);
 
-            boolean isCallerNumberValid = checkForValidPhoneNumber(commandLineArgs.get(5));
+            boolean isCallerNumberValid = HelperFunctions.checkForValidPhoneNumber(commandLineArgs.get(5));
             if (!isCallerNumberValid) {
                 return false;
             }
-            boolean isCalleeNumberValid = checkForValidPhoneNumber(commandLineArgs.get(6));
+            boolean isCalleeNumberValid = HelperFunctions.checkForValidPhoneNumber(commandLineArgs.get(6));
             if (!isCalleeNumberValid) {
                 return false;
             }
@@ -390,25 +286,6 @@ public class Project3 {
         }
         return true;
     }
-
-//    /*************
-//     * This method displays the missing args message
-//     *************/
-//    public static void printThisUsageMessage()
-//    {
-//        System.out.println("Usage: java edu.pdx.cs410J.<login-id>.Project3 [options] <args> args are (in this order):");
-//        System.out.println("customer --> Person whose phone bill we’re modeling");
-//        System.out.println("callerNumber --> Phone number of caller");
-//        System.out.println("calleeNumber --> Phone number of person who was called");
-//        System.out.println("begin --> Date and time (am/pm) call began");
-//        System.out.println("end --> Date and time (am/pm) call ended");
-//        System.out.println("Options are (options may appear in any order):");
-//        System.out.println("-pretty file --> Pretty print the phone bill to a text file or standard out (file -)");
-//        System.out.println("-textFile file --> Where to read/write the phone bill");
-//        System.out.println("-print --> Prints a description of the new phone call");
-//        System.out.println("-README --> Prints a README for this project and exits");
-//        System.exit(1);
-//    }
 
     /** checkForValidPhoneCallTime() method is used to describe the correctness of the Time specified for a
      * Phone Call created
@@ -443,40 +320,6 @@ public class Project3 {
             return true;
         }
         return true;
-    }
-
-    /** checkForValidPhoneNumber() checks the correctness of the entered Phone Number in the
-     * correct format
-     *
-     * @param phoneNumber describes the Phone Number of customer who is making efforts to make a Phone Call
-     * @return the Phone Number for the customer
-     */
-    static boolean checkForValidPhoneNumber(String phoneNumber) {
-        String regPhoneNumber = "\\d\\d\\d-\\d\\d\\d-\\d\\d\\d\\d";
-        boolean validNumberOfCaller = Pattern.compile(regPhoneNumber).matcher(phoneNumber).matches();
-        if (!validNumberOfCaller) {
-            String invalidPhoneNumberMessage = "Phone Number provided is invalid, please retry by entering the correct one's";
-            System.out.println(invalidPhoneNumberMessage);
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     *The readMeFlagCheck() tells whether optional -README flag has been entered at Command Line
-     */
-    static boolean readMeFlagCheck(String[] args) {
-        int countIdxForREADME = 0;
-        for (String arg : args) {
-            if (countIdxForREADME > 1) {
-                break;
-            } else if (arg.toUpperCase().contains("README")) {
-                return true;
-            } else {
-                countIdxForREADME++;
-            }
-        }
-        return false;
     }
 
     /**
