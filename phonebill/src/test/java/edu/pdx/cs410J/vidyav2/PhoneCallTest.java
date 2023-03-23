8c9aaca084652c8bbddbe451e9ff8fd3f86cc671
vidyav2 <hjvidyavarshini@gmail.com>
2023-03-23 09:14:13 -0700

    PhoneCallTest - added new testcases

--- phonebill/src/test/java/edu/pdx/cs410J/vidyav2/PhoneCallTest.java
+++ phonebill/src/test/java/edu/pdx/cs410J/vidyav2/PhoneCallTest.java
@@ -1,11 +1,10 @@
 package edu.pdx.cs410J.vidyav2;
 
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.Date;
-//
-//import static org.hamcrest.CoreMatchers.*;
-//import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit tests for the {@link PhoneCall} class.
@@ -14,50 +13,30 @@ package edu.pdx.cs410J.vidyav2;
  */
 public class PhoneCallTest {
 
-//    String callerNumber = "123-777-9997";
-//    String calleeNumber = "888-998-9999";
-//
-//
-//    Date phoneCallBeginTime = new Date("07/07/2021 08:00 PM"), phoneCallEndTime = new Date("07/07/2021 08:05 PM");
-//    String callerName = "Bhaskar";
-//    String phoneCallBeginDate;
-//            //= "07/07/2022";
-//    String phoneCallEndDate;
-//                    //= "07/07/2022";
-//
-//  PhoneCall call = new PhoneCall(callerName, callerNumber, calleeNumber, phoneCallBeginDate,phoneCallBeginTime, phoneCallEndDate,phoneCallEndTime);
-//
-//  /**
-//   * This unit test will need to be modified (likely deleted) as you implement
-//   * your project.
-//   */
-//
-//  @Test
-//  void getCallerIsImplemented() {
-//    assertThat(call.getCaller(), equalTo(callerNumber));
-//  }
-//
-//  @Test
-//  void getCalleeIsImplemented() {
-//    assertThat(call.getCallee(), equalTo(calleeNumber));
-//  }
-////  @Test
-////  void getBeginTimeStringIsImplemented() {
-////    assertThat(call.getBeginTimeString(), equalTo(phoneCallBeginTime));
-////  }
-//
-////  @Test
-////  void getEndTimeStringIsImplemented() {
-////    assertThat(call.getEndTimeString(), equalTo(phoneCallEndTime));
-////  }
-//
-//  @Test
-//  void initiallyAllPhoneCallsHaveTheSameCallee() {
-//    assertThat(call.getCallee(), equalTo(calleeNumber));
-//  }
-//
-//  @Test
-//  void forProject1ItIsOkayIfGetBeginTimeReturnsNull() {
-//    assertThat(call.getBeginTime(), is(nullValue()));
-//  }
+    /**
+     * This unit test will need to be modified (likely deleted) as you implement
+     * your project.
+     */
+    @Test
+    void getBeginTimeStringNeedsToBeImplemented() {
+        PhoneCall call = new PhoneCall();
+        assertThrows(UnsupportedOperationException.class, call::getBeginTimeString);
+    }
+
+    /**
+     * This unit test will need to be modified (likely deleted) as you implement
+     * your project.
+     */
+    @Test
+    void initiallyAllPhoneCallsHaveTheSameCallee() {
+        PhoneCall call = new PhoneCall();
+        assertThat(call.getCallee(), containsString("not implemented"));
+    }
+
+    @Test
+    void forProject1ItIsOkayIfGetBeginTimeReturnsNull() {
+        PhoneCall call = new PhoneCall();
+        assertThat(call.getBeginTime(), is(nullValue()));
+    }
+
 }
