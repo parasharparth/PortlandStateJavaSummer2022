0b5e2552e58737a569f759ff4927e2aea7f14bbe
Annie Pathania <pathania@pdx.edu>
2023-03-23 18:58:56 -0700

    Modifications and new updates(Documentation) done in PhoneCall.java file

--- phonebill/src/main/java/edu/pdx/cs410J/vidyav2/PhoneCall.java
+++ phonebill/src/main/java/edu/pdx/cs410J/vidyav2/PhoneCall.java
@@ -7,35 +7,56 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-/************************************************
- * This is a PhoneCall class extended by AbstractPhoneCall
- * and implements Comparable
- ************************************************/
+/************************************************************************************
+ * This is a PhoneCall class extended by AbstractPhoneCall and implements Comparable
+ * Defines the details and date specified in a call
+ ************************************************************************************/
 public class PhoneCall extends AbstractPhoneCall implements Comparable<PhoneCall> {
-  /************************************************
-   * Defines the details specified in a call
-   ************************************************/
+
   String callerNumber, calleeNumber, phoneCallBeginDate, phoneCallEndDate, callerName;
-  /************************************************
-   * Defines the date specified in a call
-   ************************************************/
   Date phoneCallBeginTime, phoneCallEndTime;
 
-  /***********************************************
-  * PhoneCall method to specify the phone call
-   * *********************************************/
+  /**************************************************************************************
+   * PhoneCall method to specify the phone call
+   * PhoneCall constructor to specify the phone call
+   * calleeNumber--callee number is called
+   * callerName--caller name id called
+   * callerNumber--caller number id called
+   * phoneCallBeginDate--call begin date
+   * phoneCallBeginTime--call begin time
+   * phoneCallEndDate--call end date
+   * phoneCallEndTime--call end time
+   * getCaller() method is used to retrieve the caller details
+   * getCallee() method is used to retrieve the caller details
+   * getBeginTimeString() method is used to retrieve the caller details
+   * getEndTimeString() method is used to retrieve the caller details
+   * setCallerName() method is used to set phone call to the phone bill
+   * callerName defines the caller name
+   * setCallerNumber() method is used to set phone call to the phone bill
+   * callerNumber--defines the caller number
+   * calleeNumber() method is used to set phone call to the phone bill
+   * calleeNumber--defines the caller number
+   * setPhoneCallBeginTime() method is used to set phone call to the phone bill
+   * phoneCallBeginDate--defines call begin date
+   * phoneCallBeginTime--defines call begin time
+   * AMPM--defines am, pm
+   * setPhoneCallBeginDate() self defined
+   * phoneCallBeginDate--self defined
+   * setPhoneCallEndTime() method is used to set phone call to the phone bill
+   * phoneCallEndDate--defines call end date
+   * phoneCallEndTime--defines call end time
+   * setPhoneCallEndDate() method is self defined
+   * phoneCallEndDate--defines call end date
+   * getPhoneCallBeginDate() method is self defined
+   * getPhoneCallBeginTime() method is self defined
+   * getPhoneCallEndDate() method is self defined
+   * getPhoneCallEndTime() method is self defined
+   * compareTo()--Defines the comparable for Phonecall
+   * o the object to be compared.
+   *****************************************************************************************/
+
   public PhoneCall() {}
 
-  /*******
-   * PhoneCall constructor to specify the phone call
-   * @param calleeNumber  callee number is called
-   * @param callerName caller name id called
-   * @param callerNumber caller number id called
-   * @param phoneCallBeginDate call begin date
-   * @param phoneCallBeginTime call begin time
-   * @param phoneCallEndDate call end date
-   * @param phoneCallEndTime call end time
-   * *******/
   PhoneCall(String callerName, String callerNumber, String calleeNumber, String phoneCallBeginDate,
                   Date phoneCallBeginTime, String phoneCallEndDate, Date phoneCallEndTime){
     this.callerName = callerName;
@@ -50,19 +71,19 @@ public class PhoneCall extends AbstractPhoneCall implements Comparable<PhoneCall
   public PhoneCall(String custName, String noOfCaller, String noOfCallee, String dateOfPhoneCallBegin, String timeOfPhoneCallBegin, String dateOfPhoneCallEnd, String timeOfPhoneCallEnd) {
   }
 
-  /** getCaller() method is used to retrieve the caller details **/
+
   @Override
   public String getCaller() {
     return callerNumber;
   }
 
-  /** getCallee() method is used to retrieve the caller details **/
+
   @Override
   public String getCallee() {
     return calleeNumber;
   }
 
-  /** getBeginTimeString() method is used to retrieve the caller details **/
+
   @Override
   public String getBeginTimeString() {
     new SimpleDateFormat("MM/dd/yyyy hh:mm aa", Locale.US);
@@ -71,7 +92,7 @@ public class PhoneCall extends AbstractPhoneCall implements Comparable<PhoneCall
     return phoneCallBeginDate + " " + timestring;
   }
 
-  /** getEndTimeString() method is used to retrieve the caller details **/
+
   @Override
   public String getEndTimeString() {
     new SimpleDateFormat("MM/dd/yyyy hh:mm aa", Locale.US);
@@ -79,33 +100,19 @@ public class PhoneCall extends AbstractPhoneCall implements Comparable<PhoneCall
   return phoneCallEndDate + " " + timestring;
   }
 
-  /********************
-   * setCallerName() method is used to set phone call to the phone bill
-   * @param callerName defines the caller name
-   *********************/
+
   public void setCallerName(String callerName) {
     this.callerName = callerName;
   }
 
-  /********************
-   * setCallerNumber() method is used to set phone call to the phone bill
-   * @param callerNumber defines the caller number
-   *********************/
+
   public void setCallerNumber(String callerNumber) {
     this.callerNumber = callerNumber; }
 
-  /********************
-   * calleeNumber() method is used to set phone call to the phone bill
-   * @param calleeNumber defines the caller number
-   *********************/
+  /
   public void setCalleeNumber(String calleeNumber) { this.calleeNumber = calleeNumber;}
 
-  /********************
-   * setPhoneCallBeginTime() method is used to set phone call to the phone bill
-   * @param phoneCallBeginDate defines call begin date
-   * @param phoneCallBeginTime defines call begin time
-   * @param AMPM defines am, pm
-   *********************/
+
   public void setPhoneCallBeginTime(String phoneCallBeginDate, String phoneCallBeginTime, String AMPM) {
     String finaldatetime = phoneCallBeginDate + " " + phoneCallBeginTime + " " + AMPM;
     SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm aa");
@@ -118,20 +125,12 @@ public class PhoneCall extends AbstractPhoneCall implements Comparable<PhoneCall
     }
   }
 
-  /********************
-   * setPhoneCallBeginDate() self defined
-   * @param phoneCallBeginDate self defined
-   *********************/
+
   public void setPhoneCallBeginDate(String phoneCallBeginDate) {
     this.phoneCallBeginDate = phoneCallBeginDate;
   }
 
-  /********************
-   * setPhoneCallEndTime() method is used to set phone call to the phone bill
-   * @param phoneCallEndDate defines call end date
-   * @param phoneCallEndTime defines call end time
-   * @param AMPM defines am, pm
-   *********************/
+
   public void setPhoneCallEndTime(String phoneCallEndDate, String phoneCallEndTime, String AMPM) {
     String finaldatetime1 = phoneCallEndDate + " " + phoneCallEndTime + " " + AMPM;
     SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm aa");
@@ -144,49 +143,32 @@ public class PhoneCall extends AbstractPhoneCall implements Comparable<PhoneCall
     }
   }
 
-  /********************
-   * setPhoneCallEndDate() method is self defined
-   * @param phoneCallEndDate defines call end date
-   *********************/
+
   public void setPhoneCallEndDate(String phoneCallEndDate) {
     this.phoneCallEndDate = phoneCallEndDate;
   }
 
-  /********************
-   * getPhoneCallBeginDate() method is self defined
-   * @return phoneCallBeginDate
-   *********************/
+
   public String getPhoneCallBeginDate() {
     return phoneCallBeginDate;
   }
 
-  /********************
-   * getPhoneCallBeginTime() method is self defined
-   * @return phoneCallBeginTime
-   *********************/
+
   public Date getPhoneCallBeginTime() {
     return this.phoneCallBeginTime;
   }
 
-  /********************
-   * getPhoneCallEndDate() method is self defined
-   * @return phoneCallEndDate
-   *********************/
+
   public String getPhoneCallEndDate() {
     return phoneCallEndDate;
   }
 
-  /********************
-   * getPhoneCallEndTime() method is self defined
-   * @return phoneCallEndTime
-   *********************/
+
   public Date getPhoneCallEndTime() {
     return this.phoneCallEndTime;
   }
 
-  /** Defines the comparable for Phonecall
-   * @param o the object to be compared.
-   */
+
   public int compareTo(PhoneCall o) {
     SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm aa");
       try {
