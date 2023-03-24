298506eb387d865639adb6198babc6df00a78c8e
vidyav2 <hjvidyavarshini@gmail.com>
2023-03-24 12:13:45 -0700

    Added unit test cases for TextParser.

--- phonebill/src/test/java/edu/pdx/cs410J/vidyav2/TextParserTest.java
+++ phonebill/src/test/java/edu/pdx/cs410J/vidyav2/TextParserTest.java
@@ -1,39 +1,37 @@
 package edu.pdx.cs410J.vidyav2;
 
 import edu.pdx.cs410J.ParserException;
-import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TextParserTest {
+      @Test
+  void validTextFileCanBeParsed() {
+    InputStream resource = getClass().getResourceAsStream("valid-phonebill.txt");
+    assertThat(resource, notNullValue());
 
-//    @Test
-//    void validTextFileCanBeParsed() throws ParserException {
-//    InputStream resource = getClass().getResourceAsStream("/Users/vidyavarshinihj/PortlandStateJavaSummer2022/phonebill/src/test/resources/edu.pdx.cs410J.vidyav2/valid-phonebill.txt");
-//    assertThat(resource, equalTo(null));
-//
-//        TextParser parser = new TextParser(new InputStreamReader(resource));
-//    PhoneBill bill = parser.parse();
-//    assertThat(bill.getCustomer(), equalTo("Test Phone Bill"));
-//
-// }
-//
-// @Test
-// void invalidTextFileThrowsParserException() {
-// InputStream resource = getClass().getResourceAsStream("empty-phonebill.txt");
-// assertThat(resource, notNullValue());
-//
-// TextParser parser = new TextParser(new InputStreamReader(resource));
-// assertThrows(ParserException.class, parser::parse);
-// }
+    TextParser parser = new TextParser((FileReader) new InputStreamReader(resource));
+    PhoneBill bill = parser.parse();
+    assertThat(bill.getCustomer(), equalTo(""));
+  }
+
+    @Test
+    void invalidTextFileThrowsParserException() {
+        InputStream resource = getClass().getResourceAsStream("empty-phonebill.txt");
+        assertThat(resource, notNullValue());
+
+        TextParser parser = new TextParser((FileReader) new InputStreamReader(resource));
+        assertThrows(ParserException.class, parser::parse);
+    }
 }
+
 
 
 
