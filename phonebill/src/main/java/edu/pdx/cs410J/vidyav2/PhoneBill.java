6debabd67b5cd7de18a4a88818c5166030b72cfa
Annie Pathania <pathania@pdx.edu>
2023-03-24 01:00:28 -0700

    New Documentation added in PhoneBill.java file.

--- phonebill/src/main/java/edu/pdx/cs410J/vidyav2/PhoneBill.java
+++ phonebill/src/main/java/edu/pdx/cs410J/vidyav2/PhoneBill.java
@@ -3,62 +3,47 @@ package edu.pdx.cs410J.vidyav2;
 import edu.pdx.cs410J.AbstractPhoneBill;
 import java.util.ArrayList;
 
-/************************************************
+/*************************************************
  * This is a PhoneBill class
+ * Customer being used to get customer from the main method
+ * Arraylist for calls
  ************************************************/
 public class  PhoneBill extends AbstractPhoneBill<PhoneCall> {
 
-  /**********************************************************
-   * Customer being used to get customer from the main method
-   **********************************************************/
   private String customer;
-
-  /*************************
-   *Arraylist for calls
-   *************************/
 
   private final ArrayList<PhoneCall> calls = new ArrayList<>();
 
-  /********************
-   * @param customer this is a customer method used to define the specified customer
-   * for the PhoneBill method
-   ********************/
+  /***********************************************************************************************
+   * PhoneBill()--is a customer method used to define the specified customer
+   * getCustomer() method is used to retrieve the customer details
+   * setCustomer() method is used to set the customer details
+   * addPhoneCall() method is used to add phone call to the phone bill
+   * getPhoneCalls() collection manipulates the phone call to fetch the phone call details
+   ***********************************************************************************************/
+
+
   public PhoneBill(String customer) {
      this.customer = customer;
   }
-
-  /********************
-   * getCustomer() method is used to retrieve the customer details
-  *********************/
 
   @Override
   public String getCustomer() {
     return this.customer;
   }
 
-  /********************
-   * setCustomer() method is used to set the customer details
-   * @param customer this is a customer method used to define the specified customer
-   * for the PhoneBill method
-   * @return return the customer data
-   *********************/
+
   public String setCustomer(String customer){
     //return customer;
     return this.customer = customer;
   }
 
-  /********************
-   * addPhoneCall() method is used to add phone call to the phone bill
-   * @param call this is a call method used to define the calls printed in a PhoneBill
-   *********************/
   @Override
   public void addPhoneCall(PhoneCall call) {
     this.calls.add(call);
   }
 
-  /********************
-   * getPhoneCalls() collection manipulates the phone call to fetch the phone call details
-   *********************/
+
   @Override
   public ArrayList<PhoneCall> getPhoneCalls() {
     return calls;
