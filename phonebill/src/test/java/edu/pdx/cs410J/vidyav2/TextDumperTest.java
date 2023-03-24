dc749edf5b33f95f97d8ea00ee176cb4f6166db8
vidyav2 <hjvidyavarshini@gmail.com>
2023-03-24 10:46:30 -0700

    Wrote unit test cases for TextDumperTest.java

--- phonebill/src/test/java/edu/pdx/cs410J/vidyav2/TextDumperTest.java
+++ phonebill/src/test/java/edu/pdx/cs410J/vidyav2/TextDumperTest.java
@@ -1,9 +1,15 @@
 package edu.pdx.cs410J.vidyav2;
 
+import edu.pdx.cs410J.ParserException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
 import java.io.*;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TextDumperTest {
 
@@ -19,4 +25,49 @@ public class TextDumperTest {
     sw.write(bill.getCustomer());
     String text = sw.toString();
     assertThat(text, containsString(customer));
-  }}
+  }
+
+  @Test
+  public void testDump() {
+    // Prepare test data
+    PhoneBill bill = new PhoneBill("Test Customer");
+    PhoneCall call1 = new PhoneCall();
+    PhoneCall call2 = new PhoneCall();
+    bill.addPhoneCall(call1);
+    bill.addPhoneCall(call2);
+
+    // Set up test environment
+    String filename = "testDump.txt";
+    File file = new File(filename);
+    if (file.exists()) {
+      file.delete();
+    }
+
+    // Call the method under test
+    StringWriter sw = new StringWriter();
+    TextDumper dumper = new TextDumper(sw);
+    dumper.setFileName("vidyav2/testDumper.txt");
+    dumper.dump(bill);
+
+    // Verify the results
+    assertTrue(file.exists());
+
+    String text = sw.toString();
+    assertThat(text, containsString(String.valueOf(call1.getBeginTime())));
+  }
+  @Test
+  void ParseTextWrittenByTextDumper(@TempDir File tempDir) throws IOException, ParserException {
+    String customer = "Test Phone Bill";
+    PhoneBill bill = new PhoneBill(customer);
+
+    File textFile = new File(tempDir, "apptbook.txt");
+    TextDumper dumper = new TextDumper(new FileWriter(textFile));
+    dumper.dump(bill);
+
+    Reader reader = null;
+    TextParser parser = new TextParser(new FileReader(textFile), null);
+    PhoneBill read = parser.parse();
+    assertThat(read.getCustomer(), equalTo(customer));
+  }
+}
+
