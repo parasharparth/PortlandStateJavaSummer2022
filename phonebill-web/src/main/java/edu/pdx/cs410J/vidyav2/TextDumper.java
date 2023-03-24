a21978ceb8868ffc0642d72f7c039ae514958729
parasharparth <parthparashar6@gmail.com>
2023-03-24 13:56:44 -0700

    TextDumper changed

--- phonebill-web/src/main/java/edu/pdx/cs410J/vidyav2/TextDumper.java
+++ phonebill-web/src/main/java/edu/pdx/cs410J/vidyav2/TextDumper.java
@@ -5,43 +5,31 @@ import java.io.*;
 import java.util.*;
 
 public class TextDumper implements PhoneBillDumper<PhoneBill> {
-    private final Writer writer;
     public String filename;
     public TextDumper(Writer writer) {
-        this.writer = writer;
     }
 
     public TextDumper()
     {
-        this.writer = null;
     }
 
     @Override
-    public void dump(PhoneBill bill) throws  IOException{
-        ArrayList phonecallList = (ArrayList) bill.getPhoneCalls();
-        String[] calls = new String[phonecallList.size()];
-        //File f = new File(filename);
-        //FileReader fr = new FileReader(f);
+    public void dump(PhoneBill bill) {
+        ArrayList phoneCallList = (ArrayList) bill.getPhoneCalls();
+        String[] calls = new String[phoneCallList.size()];
         try {
             PrintWriter out = new PrintWriter(filename);
             out.write("");
             out.write(bill.getCustomer());
 
-            Collections.sort(phonecallList);
-            ArrayList<String> lines = new ArrayList<String>();
-            for (int i = 0; i < phonecallList.size(); i++) {
-                calls[i] = phonecallList.get(i).toString();
+            Collections.sort(phoneCallList);
+            for (int i = 0; i < phoneCallList.size(); i++) {
+                calls[i] = phoneCallList.get(i).toString();
                 out.write("\n");
                 out.write(calls[i]);
-                //out.write(lines.get(i));
                 out.flush();
-                //out.close();
-
             }
-//            for (int i = 0; i < phonecallList.size(); i++) {
-//                out.flush();
-//                out.write(calls[i]);
-//            }
+
         }
         catch(Exception e)
         {
