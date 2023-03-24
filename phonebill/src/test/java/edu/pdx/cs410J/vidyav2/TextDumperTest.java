9968237774476189217de4a62c5e36992fff2787
vidyav2 <hjvidyavarshini@gmail.com>
2023-03-24 10:31:50 -0700

    Code clean up - TextDumperTest.java

--- phonebill/src/test/java/edu/pdx/cs410J/vidyav2/TextDumperTest.java
+++ phonebill/src/test/java/edu/pdx/cs410J/vidyav2/TextDumperTest.java
@@ -8,8 +8,8 @@ import static org.hamcrest.Matchers.containsString;
 public class TextDumperTest {
 
   @Test
-  void appointmentBookOwnerIsDumpedInTextFormat() throws IOException {
-    File file = new File("vidyav2/testDumper.txt");
+  void appointmentBookOwnerIsDumpedInTextFormat() {
+    new File("vidyav2/testDumper.txt");
     String customer = "Test Phone Bill1";
     PhoneBill bill = new PhoneBill(customer);
     StringWriter sw = new StringWriter();
@@ -19,25 +19,4 @@ public class TextDumperTest {
     sw.write(bill.getCustomer());
     String text = sw.toString();
     assertThat(text, containsString(customer));
-    //File file = new File("vidyav2/testDumper.txt");
-    //if (file.exists()) {
-      //file.delete();
-    //}
   }}
-
-
-
-//  @Test
-//  void phoneBillIsDumpedInTextFormat() {
-//    String customer = "Test Phone Bill";
-//    PhoneBill bill = new PhoneBill(customer);
-//
-//    StringWriter sw = new StringWriter();
-//    TextDumper dumper = new TextDumper(sw);
-//    dumper.setFileName("bogus.txt");
-//    dumper.dump(bill);
-//    sw.write(bill.getCustomer());
-//
-//    String text = sw.toString();
-//    assertThat(text, containsString(customer));
-//  }
