dc9fa9222fc69dfda0fd559644407ba1a30f47bf
Annie Pathania <pathania@pdx.edu>
2023-03-24 19:27:19 -0700

    New documentation done in Phonebill-android (DeleteActivity.java) file

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/DeleteActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/DeleteActivity.java
@@ -10,8 +10,24 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 
+/******************************************************************************
+ * This allows the user to delete a phone call record for a specified customer.
+ ******************************************************************************/
 public class DeleteActivity extends AppCompatActivity {
 
+    /*********************************************************************************************************
+     * Called when the activity is created. This method sets the content view to the layout
+     * defined in activity_delete.xml.
+     * @param savedInstanceState If the activity is being re-initialized after previously being
+     * shut down, then this Bundle contains the data it most recently
+     * supplied in onSaveInstanceState(Bundle). Otherwise, it is null.
+     * deletephonecall() This method is called when the "Delete Phonecall" button is clicked
+     * Find the EditText view that contains the customer name
+     * Check if the customer name is empty
+     * Get the directory where the phone call files are stored
+     * If the file does not exist, show a snackbar message indicating that the phone call does not exist
+     * Clear the EditText view that contains the customer name
+     *********************************************************************************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
