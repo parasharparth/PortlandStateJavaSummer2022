5ba3f273eb4335a15b0063e2aff2be96dbcb371d
Annie Pathania <pathania@pdx.edu>
2023-03-24 19:30:39 -0700

    Phonebill-android (PrettyPrinter.java) file updated

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/PrettyPrinter.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/PrettyPrinter.java
@@ -6,10 +6,18 @@ import java.util.concurrent.TimeUnit;
 
 import edu.pdx.cs410J.PhoneBillDumper;
 
+/*************************************************************************************************************
+ * Defines a class named PrettyPrinter that implements the PhoneBillDumper interface for the PhoneBill class.
+ *************************************************************************************************************/
 public class PrettyPrinter implements PhoneBillDumper<PhoneBill> {
-    /**
+
+    /*******************************************************************************************************************
      * This method formats the phone-bill in a pretty way
-     */
+     * Compute the duration of the phone call in milliseconds
+     * Construct a string with the formatted phone call information
+     * Return the string containing the formatted phone call information
+     * dump() This method does nothing since there is no requirement to dump an entire phone bill in a pretty way
+     *******************************************************************************************************************/
     public String getpretty(PhoneCall call, String customer) {
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
         long callDuration = call.getPhoneCallEndTime().getTime() - call.getPhoneCallBeginTime().getTime();
