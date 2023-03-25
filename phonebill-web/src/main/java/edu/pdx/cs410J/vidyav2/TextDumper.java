31b3b8a95b0c387c189864fb799af6d7bd874a67
vidyav2 <hjvidyavarshini@gmail.com>
2023-03-24 18:47:26 -0700

    TextDumper - refactored the code

--- phonebill-web/src/main/java/edu/pdx/cs410J/vidyav2/TextDumper.java
+++ phonebill-web/src/main/java/edu/pdx/cs410J/vidyav2/TextDumper.java
@@ -5,12 +5,12 @@ import java.io.*;
 import java.util.*;
 
 public class TextDumper implements PhoneBillDumper<PhoneBill> {
-    public String filename;
+    String filename;
+
     public TextDumper(Writer writer) {
     }
 
-    public TextDumper()
-    {
+    public TextDumper() {
     }
 
     public static long formatDefinition(String test, String thisIsATestDefinition) {
@@ -19,32 +19,25 @@ public class TextDumper implements PhoneBillDumper<PhoneBill> {
 
     @Override
     public void dump(PhoneBill bill) {
-        ArrayList phoneCallList = (ArrayList) bill.getPhoneCalls();
-        String[] calls = new String[phoneCallList.size()];
-        try {
-            PrintWriter out = new PrintWriter(filename);
+        ArrayList<PhoneCall> phoneCallList = (ArrayList<PhoneCall>) bill.getPhoneCalls();
+        try (PrintWriter out = new PrintWriter(filename)) {
             out.write("");
             out.write(bill.getCustomer());
 
             Collections.sort(phoneCallList);
-            for (int i = 0; i < phoneCallList.size(); i++) {
-                calls[i] = phoneCallList.get(i).toString();
-                out.write("\n");
-                out.write(calls[i]);
-                out.flush();
+            StringBuilder calls = new StringBuilder();
+            for (PhoneCall phoneCall : phoneCallList) {
+                calls.append("\n").append(phoneCall.toString());
             }
-
-        }
-        catch(Exception e)
-        {
+            out.write(calls.toString());
+            out.flush();
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
         }
     }
 
-    public void setFileName(String filename)
-    {
+    void setFileName(String filename) {
         this.filename = filename;
     }
-
 }
