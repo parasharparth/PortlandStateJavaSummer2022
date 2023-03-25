e0ec8ea7807ea3977b9614087e15e9f857e25a69
Annie Pathania <pathania@pdx.edu>
2023-03-24 19:31:09 -0700

    Phonebill-android (readmeactivity.java) file updated

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/readmeactivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/readmeactivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/*********************************************************************************
+ * Declare the public class 'readmeactivity' which extends 'AppCompatActivity'
+ *********************************************************************************/
 public class readmeactivity extends AppCompatActivity {
 
+    /****************************************************************************************
+     * Override the 'onCreate' method of 'AppCompatActivity'
+     * Set the content view to be the XML layout defined in 'activity_readme_activity.xml'
+     ****************************************************************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
